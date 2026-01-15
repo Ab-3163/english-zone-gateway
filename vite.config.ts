dcf869175ef798a7c7cf799eef862d2753eaabdf
Ab-3163 <khairyabdou027@gmail.com>
2026-01-15 15:05:41 +0000

    fix base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/english-zone-gateway/",
   server: {
     host: "::",
     port: 8080,
